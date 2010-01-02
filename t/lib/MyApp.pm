dc7d4c354379a49c8dc273147ab2eefd5fae2271
Moritz Onken <onken@netcubed.de>
2010-01-02 18:55:23 +0000

    Import of PERLER/CatalystX-Test-Recorder-0.99 from CPAN.
    gitpan-cpan-distribution: CatalystX-Test-Recorder
    gitpan-cpan-version:      0.99
    gitpan-cpan-path:         PERLER/CatalystX-Test-Recorder-0.99.tar.gz
    gitpan-cpan-author:       PERLER
    gitpan-cpan-maturity:     released

---
+++ t/lib/MyApp.pm
@@ -0,0 +1,7 @@
+package MyApp;
+our $VERSION = '0.99';
+use Moose;
+extends 'Catalyst';
+__PACKAGE__->config( 'CatalystX::Test::Recorder' => { skip => [qr/^static/], namespace => 'recorder' } );
+__PACKAGE__->setup(qw(+CatalystX::Test::Recorder));
+1;
---
+++ t/lib/MyAppT.pm
@@ -0,0 +1,7 @@
+package MyAppT;
+our $VERSION = '0.99';
+use Moose;
+extends 'Catalyst';
+__PACKAGE__->config( { 'CatalystX::Test::Recorder' => { template => 't/src/template.tt', namespace => 'foobar' }, setup_components => {search_extra => ['MyApp']} }  );
+__PACKAGE__->setup(qw(+CatalystX::Test::Recorder));
+1;
